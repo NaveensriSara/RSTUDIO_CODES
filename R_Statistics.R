ac6ea51630e0b9ee1b74c6165c703d742cf77776
NaveensriSara <111405332+NaveensriSara@users.noreply.github.com>
2024-02-27 23:52:54 -0500

    Add files via upload

---
+++ R_Statistics.R
@@ -0,0 +1,3 @@
+#Looking at Stroke dataset from Kaggle and looking through basic Statistics 
+Stroke_data <- read.csv("/Users/naveensrisaravanan/Documents/GitHub/Rstudio_codes/healthcare-dataset-stroke-data.csv")
+view(Stroke_data)
